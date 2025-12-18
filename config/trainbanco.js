ff3defe503f2ec33404bd3144bfada8dca1cdba5
Sergio Ricardo <sergioricardobr013@gmail.com>
2025-12-18 16:04:52 -0300

    traindbanco

--- config/trainbanco.js
+++ config/trainbanco.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const Banco = new Sequelize("mytrain", "root", "Sergio12!", {
+const Banco = new Sequelize("mytrain", "aluno", "ifpecjbg", {
     host: "localhost",
     dialect: "mysql",
     logging: false
